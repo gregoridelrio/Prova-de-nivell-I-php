b0413a6980e1da7f73c43a484c43b2c333bbbcd0
Laurea Gregori <gregoridelrio@gmail.com>
2026-01-15 11:14:10 +0100

    Refactor: giving more value to red cards

--- Equipo.php
+++ Equipo.php
@@ -2,6 +2,7 @@
 class Equipo
 {
   private array $jugadores = [];
+  private int $valorRojas = 2;
 
   public function anadirJugador(Jugador $jugador): void
   {
@@ -25,7 +26,7 @@ class Equipo
     foreach ($this->jugadores as $jugador) {
       $tarjetasJugador = 0;
       $tarjetasJugador += $jugador->getAmarillas();
-      $tarjetasJugador += $jugador->getRojas();
+      $tarjetasJugador += $jugador->getRojas() * $this->valorRojas;
       if ($tarjetasJugador > $tarjetas) {
         $jugadorMasTarjetas = $jugador;
         $tarjetas = $tarjetasJugador;
